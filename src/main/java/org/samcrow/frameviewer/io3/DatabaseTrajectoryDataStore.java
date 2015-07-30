5860504cd0beea49738792007fa2f3b43d2ae179
Sam Crow <samcrow@uw.edu>
2015-07-29 17:39:13 -0700

    GPL

--- src/main/java/org/samcrow/frameviewer/io3/DatabaseTrajectoryDataStore.java
+++ src/main/java/org/samcrow/frameviewer/io3/DatabaseTrajectoryDataStore.java
@@ -1,3 +1,23 @@
+// <editor-fold defaultstate="collapsed" desc="License">
+/* 
+ * Copyright 2012-2015 Sam Crow
+ * 
+ * This file is part of Trajectory.
+ * 
+ * Trajectory is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Trajectory is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Trajectory.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// </editor-fold>
 package org.samcrow.frameviewer.io3;
 
 import java.io.Closeable;
